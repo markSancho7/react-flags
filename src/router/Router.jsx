49ec69c8e219e0e00823744fbd8e593c8a858df0
markSancho7 <marcos.sancho7@alumnostrazos.net>
2024-01-26 12:07:09 +0100

    actualizado

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Main from '../components/main /Main';
+import Layout from '../components/layout /Layout';
 import CountryDetails from '../pages/countryDetails/CountryDetails';
 
 const Router = () => {
 	return (
 		<Routes>
+			<Layout></Layout>
 			<Route path='/' element={<Main />}></Route>
 			<Route index element={<Main />}></Route>
 			<Route path='/countryDetails/' element={<CountryDetails />}></Route>
